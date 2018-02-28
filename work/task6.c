7e3d2cd6006a54aa4a9266e226f1676d22db0be0
SergeyPerepelkin <Seregaperepelmail@gmail.com>
2018-02-28 16:01:26 +0300

    change 6

--- work/task6.c
+++ work/task6.c
@@ -2,24 +2,16 @@
 #include <stdlib.h>
 #include "task6.h"
 
-typedef unsigned long long ull;
+
 
 unsigned long long fib2(int N)
 {
-	ull sum = 0;
-	ull num1 = 0;
-	ull num2 = 1;
-	sum = fib3(num1, num2, N);
-	return sum;
+	return fib3(0, 1, N);
 }
 
 unsigned long long fib3(unsigned long long num1, unsigned long long num2, int N)
 {
-	ull sum = num1 + num2;
-
 	if (N == 2)
-		return sum;
-	else
-		sum = fib3(num2, sum, N - 1);
-	return sum;
+		return num1 + num2;
+	return fib3(num2, num1 + num2, N - 1);
 }
